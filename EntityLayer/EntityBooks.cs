7939177b7e19cc044409d88aa9b31567c77b20ac
Büşra Alma <busraalmaa@gmail.com>
2021-05-14 23:24:57 +0300

    Add project files.

---
+++ EntityLayer/EntityBooks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    class EntityBooks
+    {
+        private int bookId;
+        private string bookName;
+        private string authorName;
+        private EntityCategory category;          //     ?
+
+        public int BookId { get => bookId; set => bookId = value; }
+        public string BookName { get => bookName; set => bookName = value; }
+        public string AuthorName { get => authorName; set => authorName = value; }
+        internal EntityCategory Category { get => category; set => category = value; }
+    }
+}
---
+++ EntityLayer/EntityUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityUser
+    {
+        private int userId;
+        private string firstName;
+        private string lastName;
+        private string username;
+        private string email;
+        private string password;
+
+        public int UserId { get => userId; set => userId = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Username { get => username; set => username = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        
+    }
+}
