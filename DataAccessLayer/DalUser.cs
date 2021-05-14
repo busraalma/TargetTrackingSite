254693a8fe0e1b9822fea0b84e734bc91ef15386
Büşra Alma <busraalmaa@gmail.com>
2021-05-15 00:45:38 +0300

    1

---
+++ DataAccessLayer/DalUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using EntityLayer;
+
+namespace DataAccessLayer
+{
+    public class DalUser
+    {
+    }
+}
